ee5f79161612052ee331b99bc646e51a8fd53d8e
YogeshP16 <yogesh010.p@gmail.com>
2024-12-16 19:35:25 +0530

    Default Constructor

---
+++ JAVA_DSA/09_OOPS/02_Constructor/Main.java
@@ -0,0 +1,15 @@
+class Person {
+    String name;
+    int age;
+
+    // No constructor defined, so default constructor is used
+    // why we use --> automatic  object creation, initialization of objects
+    // default value for string is null, int--> 0, boolean --> false
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Person p = new Person();  // Using default constructor
+        System.out.println("Name: " + p.name + ", Age: " + p.age); // Default values: null and 0
+    }
+}
